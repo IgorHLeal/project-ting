5a98cf2d3557119a426b0d79061da91f19cd5261
Igor Leal <igorhenriqueleal3@gmail.com>
2023-02-03 18:14:45 -0300

    feat: Requisito 03 - Implementando a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    path_files = [index["nome_do_arquivo"] for index in instance._data] 
+
+    if path_file not in path_files:
+        file_lines = txt_importer(path_file)
+
+        dict_files = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_lines),
+            "linhas_do_arquivo": file_lines
+        }
+
+        instance.enqueue(dict_files)
+        print(dict_files)
 
 
 def remove(instance):
