9ee7a24ba5c74fef9aed826b857d92dc9f973769
Igor Leal <igorhenriqueleal3@gmail.com>
2023-02-04 15:06:30 -0300

    feat: Requisito 06 - Implemenetando a função exists_word dentro do módulo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,27 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word, instance: Queue):
+    result = []
+
+    for index in range(len(instance)):
+        element = instance.search(index)
+
+        list_file = [
+            {"linha": index + 1}
+            for index, line in enumerate(element["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+
+        if list_file:
+            result.append({
+                "palavra": word,
+                "arquivo": element["nome_do_arquivo"],
+                "ocorrencias": list_file
+            })
+
+    return result
+
+
+def search_by_word(word, instance: Queue):
     """Aqui irá sua implementação"""
